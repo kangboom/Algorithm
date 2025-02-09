acb7ea2a94440453513a2b4633c1baa482f2e376
LimKangBeom <103165796+kangboom@users.noreply.github.com>
2025-02-10 08:47:26 +0900

    [Silver V] Title: 야구 시즌, Time: 160 ms, Memory: 15352 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/21760. 야구 시즌/야구 시즌.java
+++ 백준/Silver/21760. 야구 시즌/야구 시즌.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,25 +17,18 @@ public class Main {
 			int k = Integer.parseInt(st.nextToken());
 			int D = Integer.parseInt(st.nextToken());
 			
-			int B = D / (comb(M,2)*N*k + comb(N, 2)*M*M);
-			int answer = (comb(M,2)*N*k + comb(N, 2)*M*M)*B;
+			int B = (2*D) /( k*N*M*(M-1) + M*M*N*(N-1) );
 			
 			if(B==0) {
 				System.out.println(-1);
-				return ;
+				continue;
 			}
+			
+			int answer = (M*(M-1)/2)*N*k*B + M*M*(N*(N-1)/2)*B;
 			System.out.println(answer);
 		}
-	}
-	
-	public static int comb(int n, int r) {
-		if(r == 1) {
-			return n;
-		}
-		if(n == r) {
-			return 1;
-		}
-
-		return comb(n-1, r) + comb(n-1, r-1);
+		
 	}
 }
+
+
