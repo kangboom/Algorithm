cedbd7327c8f22dc76c3c3481136f1d23a5f0aab
LimKangBeom <103165796+kangboom@users.noreply.github.com>
2025-02-19 23:40:43 +0900

    [Silver I] Title: 부등호, Time: 144 ms, Memory: 17872 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -10,17 +10,17 @@ public class Main {
 	static int[] num = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	static long max;
 	static long min;
-	static int[] order;
-	static boolean[] visited;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 
 	public static void main(String[] args) throws IOException {
 		input();
 		for (int i = 0; i <= 9; i++) {
+			boolean[] visited = new boolean[10];
 			visited[i] = true;
+			int[] order = new int[N+1];
 			order[0] = i;
-			select(1);
+			select(1, order, visited);
 			visited[i] = false;
 		}
 		
@@ -29,7 +29,7 @@ public class Main {
 		System.out.printf("%0" + String.format("%s", N+1) + "d", min);
 	}
 
-	static void select(int depth){
+	static void select(int depth, int[] order, boolean[] visited){
 		if(depth == N+1) {
 			
 			long tmp = order[0];
@@ -47,7 +47,7 @@ public class Main {
 			
 			visited[i] = true;
 			order[depth] = i;
-			select(depth+1);
+			select(depth+1, order, visited);
 			visited[i] = false;
 		}
 	}
@@ -65,8 +65,6 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 
 		oper = new char[N];
-		order = new int[N + 1];
-		visited = new boolean[10];
 		for (int i = 0; i < N; i++) {
 			oper[i] = st.nextToken().charAt(0);
 		}
