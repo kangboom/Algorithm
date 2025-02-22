90ab45b254078a72e3f67f864cff540f48378c46
LimKangBeom <103165796+kangboom@users.noreply.github.com>
2025-02-22 17:12:36 +0900

    [Gold V] Title: 신기한 소수, Time: 100 ms, Memory: 13952 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -6,48 +6,35 @@ public class Main {
 
 	static int N;
 	static StringBuilder sb = new StringBuilder();
-	static int[] order;
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		order = new int[N];
+		N = System.in.read() + 1 - '1';
 		int[] start = {2, 3, 5, 7};
 		for(int n : start) {
-			order[0] = n;
-			solve(1);
+			solve(1, n);
 		}
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 	
-	static void solve(int depth) {
+	static void solve(int depth, int num) {
 		if(depth == N) {
-			for(int i=0; i<N; i++) {
-				sb.append(order[i]);
-			}
-			sb.append("\n");
+			sb.append(num).append("\n");;
 			return ;
 		}
 		
 		for(int i=1; i<=9; i+=2){
-			if(!isPossible(i, depth)) continue; // 소수가 아니면 pass 
-			order[depth] = i;
-			solve(depth+1);
+			int tmp = num*10 + i;
+			if(!isPossible(tmp)) continue; // 소수가 아니면 pass 
+			solve(depth+1, tmp);
 		}
 	}
 	
-	static boolean isPossible(int n, int depth) {
-		int num = 0;
-		for(int i=0; i<depth; i++) {
-			num = num*10 + order[i];
-		}
-		num = num*10 + n;
-		for(int i=2; i<=(int)Math.sqrt(num); i++) {
-			if(num % i == 0) {
+	static boolean isPossible(int n) {
+		for(int i=2; i<=(int)Math.sqrt(n); i++) {
+			if(n % i == 0) {
 				return false; 
 			}
 		}
 		return true;
 	}
-	
 }
