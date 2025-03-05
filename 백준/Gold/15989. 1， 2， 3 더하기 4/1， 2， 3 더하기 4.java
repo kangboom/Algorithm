75df055c1bcaafeae396517398feb5f97a5dfb71
LimKangBeom <103165796+kangboom@users.noreply.github.com>
2025-03-05 21:50:38 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 108 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int[][] dp = new int[10001][4];
+		dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1;
+		for(int i=4; i<=10000; i++) {
+			dp[i][1] = dp[i-1][1];
+			dp[i][2] = dp[i-2][1] + dp[i-2][2];
+			dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+		}
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			sb.append(dp[N][1] + dp[N][2] + dp[N][3]).append('\n');
+		}
+		System.out.println(sb);
+		
+	}
+
+}
