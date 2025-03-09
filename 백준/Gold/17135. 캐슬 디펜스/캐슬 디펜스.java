6fd0229950f8d81648117efc33658fb203806408
LimKangBeom <103165796+kangboom@users.noreply.github.com>
2025-03-09 22:37:52 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 176 ms, Memory: 33700 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,13 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -19,10 +14,9 @@ public class Main {
 	static int N;
 	static int M;
 	static int D;
-	static int remainCnt;
 	static int[] order;
-	static List<int[]> removeList;
-
+	static int removeCnt;
+	
 	public static void main(String[] args) throws IOException {
 		input();
 		comb(0, 0);
@@ -31,43 +25,25 @@ public class Main {
 
 	static void play() {
 		// 기존 map 복사
-		int[][] copyMap = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			copyMap[i] = Arrays.copyOf(map[i], M);
-		}
-
-		int removeCnt = 0; // 제거한 적의 개수
-		int t = 0;
-		while (t<N) {
-
-			removeList.clear();
+		removeCnt = 0; // 제거한 적의 개수
+		int depth = N;
+		int[][] status = new int[N][M];
+		while (depth>0) {
 			for (int i = 0; i < 3; i++) {
 				boolean[][] visited = new boolean[N][M];
-				bfs(N - 1-t, order[i], 1, copyMap, visited);
+				bfs(depth, order[i], visited, status); // 적 제거
 			}
-
-			for (int[] loc : removeList) {
-				if(copyMap[loc[0]][loc[1]] == 0) continue;
-				copyMap[loc[0]][loc[1]] = 0;
-				++removeCnt;
-			}
-			
-			++t;
+			--depth;
 		}
 		ans = Math.max(ans, removeCnt);
 	}
 
-	static void bfs(int y, int x, int len, int[][] copyMap, boolean[][] visited) {
-		if(copyMap[y][x] == 1) {
-			removeList.add(new int[] {y, x});
-			return ;
-		}
-		
+	static void bfs(int depth, int x, boolean[][] visited, int[][] status) {
 		int[] dy = { 0, -1, 0 };
 		int[] dx = { -1, 0, 1 };
 		
 		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] { y, x, 1 });
+		q.offer(new int[] { depth, x, 0 });
 		
 		while (!q.isEmpty()) {
 			int[] now = q.poll();
@@ -82,10 +58,13 @@ public class Main {
 				int ny = cy + dy[i];
 				int nx = cx + dx[i];
 
-				if (ny < 0 || ny > N - 1 || nx < 0 || nx > M - 1 || visited[ny][nx])
+				if (ny < 0 || ny > depth - 1 || nx < 0 || nx > M - 1 || visited[ny][nx])
 					continue;
-				if(copyMap[ny][nx] == 1) {
-					removeList.add(new int[] {ny, nx});
+				if(map[ny][nx] == 1 && status[ny][nx] == 0) {
+					++removeCnt;
+					status[ny][nx] = depth;
+					return ;
+				} else if(map[ny][nx] == 1 && status[ny][nx] == depth) {
 					return ;
 				}
 				
@@ -117,12 +96,8 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 1) {
-					++remainCnt;
-				}
 			}
 		}
-		removeList = new ArrayList<>();
 		order = new int[3];
 	}
 }
