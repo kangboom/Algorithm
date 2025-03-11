1880370b78a24ca61aa2b5733f99e47e0ccfaf5f
LimKangBeom <103165796+kangboom@users.noreply.github.com>
2025-03-12 00:04:34 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 17208 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -5,54 +5,67 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int[] lines;
 	static int N;
 	static int K;
-	static long answer;
-	static int max;
-	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static long ans;
+	static int[] lines;
+	static long max;
+
 	public static void main(String[] args) throws IOException {
-		input();
-		binarySearch(1, max);
-		System.out.println(answer);
-	}
-	
-	static void binarySearch(long low, long high) {
-		if(low>high) return;
-		
-		long mid = (low + high) / 2;
-		
-		if(validate(mid)) {
-			answer = Math.max(mid,  answer);
-			binarySearch(mid+1, high);
-		} else {
-			binarySearch(low, mid-1);
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		max = 0;
+		lines = new int[N];
+		for (int i = 0; i < N; i++) {
+			lines[i] = Integer.parseInt(br.readLine());
+			max = Math.max(max, lines[i]);
 		}
-	}
-	
-	static boolean validate(long length) {
-		int cnt = 0;
-		for(int i=0; i<K; i++) {
-			cnt += lines[i] / length;
-			if(cnt >= N) return true;
-		}
-		return false;
+
+		search();
+		System.out.println(ans);
 	}
 
-	static void input() throws IOException {
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		K = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		
-		max = 0;
-		lines = new int[K];
-		for(int i=0; i<K ;i++) {
-			lines[i] = Integer.parseInt(br.readLine());
-			max = Math.max(lines[i], max);
+//	static void search(long low, long high) {
+//		if(low > high) {
+//			return ;
+//		}
+//		
+//		long mid = (low + high) / 2;
+//		
+//		int cnt = 0;
+//		for(int line : lines) {
+//			cnt += line/mid;
+//		}
+//		if(cnt >= K) {
+//			ans = Math.max(ans, mid);
+//			search(mid+1, high);
+//		} else {
+//			search(low, mid-1);
+//		}
+//		
+//	}
+	static void search() {
+		long low = 1;
+		long high = max;
+		while (low <= high) {
+			long mid = (low + high) / 2;
+
+			int cnt = 0;
+			for (int line : lines) {
+				cnt += line / mid;
+			}
+			
+			if(cnt >= K) {
+				ans = Math.max(ans, mid);
+				low = mid+1;
+			} else {
+				high = mid-1;
+			}
 		}
-		
-		answer = 0;
 	}
 
 }
