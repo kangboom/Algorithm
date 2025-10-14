1ad01495021d33947d916ccef12aebd19d6dcb48
LimKangBeom <jkl6147@naver.com>
2025-10-14 20:14:23 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 148 ms, Memory: 17020 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,118 +1,114 @@
-import java.util.*;
-import java.io.*;
-import static java.lang.Integer.parseInt;
-public class Main {
-	static int n;
-	static ArrayList<Pair>[] graph;
-	static int[] visited;
-	static Queue<Sair> queue = new LinkedList<>();
-	
-	static boolean bfs() {
-		while (!queue.isEmpty()) {
-			Sair current = queue.poll();
-			
-			for (Pair node : graph[current.idx]) {
-				
-				// int num = (int)Math.ceil((double)node.dist / 50);
-				if (visited[node.idx] == -1 && node.dist <= current.cnt * 50) {
-					//visited[node.idx] = current.cnt - num + 20;
-					visited[node.idx] = 20;
-					
-					queue.offer(new Sair(node.idx, 20));
-				}
-			}
-		}
-		
-		return false;
-	}
-	
-	public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        int T = Integer.parseInt(br.readLine());
-        for (int t = 0; t < T; t++) {
-        	n = Integer.parseInt(br.readLine());
-        	
-        	ArrayList<int[]> list = new ArrayList<>();
-        	st = new StringTokenizer(br.readLine());
-        	int hx = Integer.parseInt(st.nextToken());
-        	int hy = Integer.parseInt(st.nextToken());
-        	
-        	list.add(new int[] {hx, hy});
-        	for (int i = 0; i < n; i++) {
-        		st = new StringTokenizer(br.readLine());
-        		
-        		int x = Integer.parseInt(st.nextToken());
-        		int y = Integer.parseInt(st.nextToken());
-        	
-        		list.add(new int[] {x, y});
-        	}
-        	
-        	st = new StringTokenizer(br.readLine());
-        	int rx = Integer.parseInt(st.nextToken());
-        	int ry = Integer.parseInt(st.nextToken());
-        	
-        	list.add(new int[] {rx, ry});
-        	
-        	graph = new ArrayList[n + 2];
-        	visited = new int[n + 2];
-        	
-        	Arrays.fill(visited, -1);
-        	for (int i = 0; i < n + 2; i++) {
-        		graph[i] = new ArrayList<>();
-        	}
-        	
-        	for (int i = 0; i < n + 2; i++) {
-        		int[] first = list.get(i);
-        		for (int j = i + 1; j < n + 2; j++) {
-        			int[] second = list.get(j);
-        			
-        			int dist = getDist(first, second);
-        			graph[i].add(new Pair(j, dist));
-        			graph[j].add(new Pair(i, dist));
-        		}
-        	}
-        	
-        	visited[0] = Integer.MAX_VALUE;
-        	queue.add(new Sair(0, 20));
-        	
-        	bfs();
-        	
-        	//System.out.println(Arrays.toString(visited));
-        	
-        	if (visited[n + 1] != -1) {
-        		System.out.println("happy");
-        	} else {
-        		System.out.println("sad");
-        	}
-        }
-        
-        
-    }
-	
-	static int getDist(int[] first, int[] second) {
-		return Math.abs(first[0] - second[0]) + Math.abs(first[1] - second[1]);
-	}
-	
-	static class Pair {
-		int idx;
-		int dist;
-		
-		public Pair(int idx, int dist) {
-			this.idx = idx;
-			this.dist = dist;
-		}
-	}
-	
-	static class Sair {
-		int idx;
-		int cnt;
-		
-		public Sair(int idx, int cnt) {
-			this.idx = idx;
-			this.cnt = cnt;
-		}
-	}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+public class Main {
+
+    static int N;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static Pos[] info;
+    static List<Integer>[] graph;
+    static int[] order = new int[2];
+    static String ans;
+    public static void main(String[] args) throws Exception {
+        int t = Integer.parseInt(br.readLine());
+        for(int i=0; i<t; i++) {
+            input();
+            comb(0,0);
+            solve();
+            System.out.println(ans);
+        }
+    }
+
+    // dfs 탐색 코드
+    static void solve(){
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(0);
+        boolean[] visited = new boolean[N+2];
+        visited[0] = true;
+
+        while(!q.isEmpty()){
+            int now = q.poll();
+
+            if(now == N+1){
+                ans="happy";
+                return;
+            }
+
+            for(int i=0; i<graph[now].size(); i++){
+                int next = graph[now].get(i);
+                if(visited[next]) continue;
+                if(isNext(info[now], info[next])){
+                	visited[next] = true;
+                    q.offer(next);
+                }
+            }
+        }
+    }
+
+    // 탐색할 다음 노드 체크, 현재애서 목적지까지의 거리보다 멀어지는 거리는 제외
+    static boolean isNext(Pos now, Pos next){
+        Pos dest = info[N+1];
+        int standard = Math.abs(dest.y-now.y) + Math.abs(dest.x - now.x);
+        int tmp = Math.abs(next.y - now.y) + Math.abs(next.x - now.x);
+        if(standard >= tmp){
+            return true;
+        }
+        return false;
+    }
+
+    static void input() throws Exception {
+        N = Integer.parseInt(br.readLine());
+        graph = new List[N+2];
+        info = new Pos[N+2];
+        for(int i=0; i<N+2; i++) {
+            graph[i] = new ArrayList<>();
+            st = new StringTokenizer(br.readLine());
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            info[i] = new Pos(y, x);
+        }
+        ans = "sad";
+    }
+
+    /*
+    그래프 그리기
+     */
+    static void comb(int start, int depth){
+        if(depth == 2){
+            int index1 = order[0];
+            int index2 = order[1];
+            Pos p1 = info[index1];
+            Pos p2 = info[index2];
+
+            // 위치 계산: 1000m 안으로 갈 수 있는 것만 그래프에 추가
+            int distance = Math.abs(p2.x - p1.x) + Math.abs(p2.y - p1.y);
+            if(distance <= 1000){
+                graph[index1].add(index2);
+                graph[index2].add(index1);
+            }
+            return ;
+        }
+
+        for(int i=start; i<N+2; i++){
+            order[depth] = i;
+            comb(i+1, depth+1);
+        }
+    }
+
+    static class Pos {
+        int y;
+        int x;
+
+        Pos(int y, int x){
+            this.y = y;
+            this.x = x;
+        }
+    }
 }
