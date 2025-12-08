66ce09eeef1c67c84275dac564b6855258ea9c11
LimKangBeom <jkl6147@naver.com>
2025-12-08 12:31:29 +0900

    [Gold IV] Title: 문자열 폭발, Time: 340 ms, Memory: 23496 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,60 +1,48 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
-public class Main {
-    
-    static String S;
-    static String T;
+public class Main { // 백준 제출 시 class 이름은 Main이어야 합니다.
     
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        S = br.readLine();
-        T = br.readLine();
+        String S = br.readLine();
+        String T = br.readLine();
         
-        int sLength = S.length();
         int tLength = T.length();
         
-        Stack<Character> stack = new Stack<>();
-        Stack<Character> tmp = new Stack<>();
+        // Stack 대신 StringBuilder를 사용하여 스택처럼 활용
+        StringBuilder sb = new StringBuilder();
         
-        for(int i=0; i<sLength; i++){
+        for (int i = 0; i < S.length(); i++) {
             char ch = S.charAt(i);
+            sb.append(ch);
             
-            if(ch == T.charAt(tLength-1) && stack.size() >= tLength-1) {
-                // 폭발 문자열 확인
-                boolean isPossible = true;
-                for (int j = tLength-2; j >=0 ; j--) {
-                    char target = stack.pop();
-                    tmp.add(target);
-                    if(target != T.charAt(j)){
-                        isPossible = false;
+            // sb의 길이가 폭발 문자열보다 길거나 같아지면 검사 시작
+            if (sb.length() >= tLength) {
+                boolean isSame = true;
+                
+                // 뒤에서부터 폭발 문자열과 일치하는지 확인
+                for (int j = 0; j < tLength; j++) {
+                    char ch1 = sb.charAt(sb.length() - tLength + j);
+                    char ch2 = T.charAt(j);
+                    
+                    if (ch1 != ch2) {
+                        isSame = false;
                         break;
                     }
                 }
                 
-                if(!isPossible){
-                    while(!tmp.isEmpty()){
-                        stack.add(tmp.pop());
-                    }
-                    stack.add(ch);
-                } else {
-                    tmp.clear();
+                // 폭발 문자열과 일치하면 뒤에서부터 길이만큼 삭제 (pop 효과)
+                if (isSame) {
+                    sb.delete(sb.length() - tLength, sb.length());
                 }
-            } else {
-                // 아니면 넣기
-                stack.add(ch);
             }
         }
         
-        StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
-        }
-        if(sb.length() != 0){
-            System.out.println(sb.reverse());
-        } else {
+        if (sb.length() == 0) {
             System.out.println("FRULA");
+        } else {
+            System.out.println(sb.toString());
         }
     }
 }
